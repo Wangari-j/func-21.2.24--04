4b61bbc8a1b4eb149c5196ff4bb06d38eb9be4e8
Wangari-j <wangarijudy@gmail.com>
2024-02-21 22:47:12 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,22 @@
+#python functions arguments
+
+def person(name):
+    print("Hello", name)
+
+    #pass argument
+person("Norah")
+person("Karen")
+person("Ckay")
+
+#functions to add two numbers
+#function add two numbers
+
+def addNumbers(num1, num2):
+    sum = num1 + num2
+    print("sum: ", + sum)
+
+    #function call with two values
+addNumbers(10,15)
+addNumbers(7,3)
+addNumbers(20,4)
+addNumbers(-3,1)
